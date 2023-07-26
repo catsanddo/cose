67ddf59ef7364f6bdd937b80db828b2e34864655
Travis Scoville <travisscoville@gmail.com>
2023-07-26 17:44:15 -0600

    Moved main include file into root directory

---
+++ cose.h
@@ -0,0 +1,17 @@
+#ifndef CE_COSE_H
+#define CE_COSE_H
+
+#include "src/types.h"
+#include "src/coro.h"
+#include "src/linked_list.h"
+#include "src/math.h"
+#include "src/arena.h"
+#include "src/string.h"
+#include "src/memory.h"
+
+#include "src/math.c"
+#include "src/arena.c"
+#include "src/string.c"
+#include "src/memory.c"
+
+#endif
